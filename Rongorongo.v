f5e57668cc5ea697e8cd0d1272415916f932a027
PortfolioAI <machineelv@gmail.com>
2026-01-14 09:04:30 -0500

    Add formalization skeleton: Barthel glyphs, boustrophedon, calendar, decidability

--- Rongorongo.v
+++ Rongorongo.v
@@ -19,3 +19,258 @@
 (*     License: MIT                                                           *)
 (*                                                                            *)
 (******************************************************************************)
+
+Require Import Coq.Arith.Arith.
+Require Import Coq.Lists.List.
+Require Import Coq.Bool.Bool.
+Require Import Lia.
+Require Import Coq.Arith.Wf_nat.
+Import ListNotations.
+
+(** * Barthel Glyph Catalog *)
+
+(** A glyph is identified by its Barthel number (001-600).
+    Core signs are ~120; rest are allographs or ligatures. *)
+Record BarthelGlyph := mkGlyph {
+  glyph_id : nat;  (* Barthel number *)
+  is_core : bool   (* true if one of ~120 core signs *)
+}.
+
+Definition valid_barthel (g : BarthelGlyph) : bool :=
+  (1 <=? glyph_id g) && (glyph_id g <=? 600).
+
+(** Ligature: compound of two or more glyphs *)
+Inductive GlyphElement :=
+  | Single : BarthelGlyph -> GlyphElement
+  | Ligature : BarthelGlyph -> BarthelGlyph -> GlyphElement.
+
+(** * Reverse Boustrophedon Reading Order *)
+
+(** Line orientation: Normal or Inverted (180° rotated) *)
+Inductive Orientation := Normal | Inverted.
+
+(** Flip orientation *)
+Definition flip_orientation (o : Orientation) : Orientation :=
+  match o with
+  | Normal => Inverted
+  | Inverted => Normal
+  end.
+
+(** A line of glyphs with its orientation *)
+Record TabletLine := mkLine {
+  line_num : nat;
+  orientation : Orientation;
+  glyphs : list GlyphElement
+}.
+
+(** A tablet side (recto or verso) *)
+Inductive Side := Recto | Verso.
+
+(** Position within a tablet *)
+Record Position := mkPos {
+  pos_side : Side;
+  pos_line : nat;
+  pos_col : nat
+}.
+
+(** A complete tablet with two sides *)
+Record Tablet := mkTablet {
+  tablet_name : nat;  (* Barthel letter encoded as number: A=1, B=2, etc. *)
+  recto_lines : list TabletLine;
+  verso_lines : list TabletLine
+}.
+
+(** Orientation of line n: alternates starting from Normal at line 0 *)
+Fixpoint line_orientation (n : nat) : Orientation :=
+  match n with
+  | O => Normal
+  | S n' => flip_orientation (line_orientation n')
+  end.
+
+(** Reading order: lines are read bottom-to-top within a side,
+    then continue to the other side. Each line alternates orientation. *)
+Definition well_formed_line (l : TabletLine) : bool :=
+  match orientation l, line_orientation (line_num l) with
+  | Normal, Normal => true
+  | Inverted, Inverted => true
+  | _, _ => false
+  end.
+
+Definition well_formed_tablet (t : Tablet) : bool :=
+  forallb well_formed_line (recto_lines t) &&
+  forallb well_formed_line (verso_lines t).
+
+(** Linearize tablet to reading order: recto lines then verso lines *)
+Definition linearize (t : Tablet) : list GlyphElement :=
+  flat_map glyphs (recto_lines t) ++ flat_map glyphs (verso_lines t).
+
+(** * Section Markers *)
+
+(** The 380.1.3 compound glyph serves as a section/paragraph marker.
+    It appears on multiple tablets (G, K, A, C, E, S) dividing text. *)
+Definition section_marker : GlyphElement :=
+  Ligature (mkGlyph 380 false) (mkGlyph 1 true).
+
+Definition is_section_marker (g : GlyphElement) : bool :=
+  match g with
+  | Ligature g1 g2 => (glyph_id g1 =? 380) && (glyph_id g2 =? 1)
+  | _ => false
+  end.
+
+(** Count section markers in a glyph sequence *)
+Definition count_sections (gs : list GlyphElement) : nat :=
+  length (filter is_section_marker gs).
+
+(** * Mamari Lunar Calendar (Tablet C) *)
+
+(** The calendar sequence on Tablet C recto lines 6-9 encodes
+    a 28-night lunar month with 8 phase divisions. *)
+Definition lunar_month_nights : nat := 28.
+Definition lunar_phases : nat := 8.
+
+(** A lunar phase marker includes moon glyph and inverted fish *)
+Record PhaseMarker := mkPhase {
+  phase_num : nat;       (* 1-8 *)
+  night_count : nat      (* nights in this phase *)
+}.
+
+(** Well-formed phase: valid phase number *)
+Definition valid_phase (p : PhaseMarker) : bool :=
+  (1 <=? phase_num p) && (phase_num p <=? lunar_phases).
+
+(** A complete lunar calendar *)
+Record LunarCalendar := mkCalendar {
+  phases : list PhaseMarker
+}.
+
+(** Calendar validity: 8 phases, total 28 nights *)
+Definition valid_calendar (c : LunarCalendar) : bool :=
+  (length (phases c) =? lunar_phases) &&
+  (fold_left (fun acc p => acc + night_count p) (phases c) 0 =? lunar_month_nights) &&
+  forallb valid_phase (phases c).
+
+(** * Parallel Text Alignment *)
+
+(** Glyph equality (by Barthel number) *)
+Definition glyph_eq (g1 g2 : BarthelGlyph) : bool :=
+  glyph_id g1 =? glyph_id g2.
+
+Definition element_eq (e1 e2 : GlyphElement) : bool :=
+  match e1, e2 with
+  | Single g1, Single g2 => glyph_eq g1 g2
+  | Ligature a1 b1, Ligature a2 b2 => glyph_eq a1 a2 && glyph_eq b1 b2
+  | _, _ => false
+  end.
+
+(** Check if sequence s1 is a subsequence of s2 *)
+Fixpoint is_subsequence (s1 s2 : list GlyphElement) : bool :=
+  match s1, s2 with
+  | [], _ => true
+  | _, [] => false
+  | x :: xs, y :: ys =>
+      if element_eq x y then is_subsequence xs ys
+      else is_subsequence s1 ys
+  end.
+
+(** Parallel passages: shared sequences across tablets *)
+Definition shares_passage (t1 t2 : Tablet) (passage : list GlyphElement) : bool :=
+  is_subsequence passage (linearize t1) &&
+  is_subsequence passage (linearize t2).
+
+(** * Decidability Results *)
+
+(** Reading order is deterministic: given a position,
+    the next position is uniquely determined *)
+Lemma flip_flip : forall o, flip_orientation (flip_orientation o) = o.
+Proof.
+  destruct o; reflexivity.
+Qed.
+
+(** Line orientation alternates predictably *)
+Lemma orientation_alternates : forall n,
+  line_orientation (S n) = flip_orientation (line_orientation n).
+Proof.
+  intros n. simpl. reflexivity.
+Qed.
+
+(** Orientation at even lines is Normal, odd lines is Inverted *)
+Lemma even_line_normal : forall n,
+  Nat.even n = true -> line_orientation n = Normal.
+Proof.
+  induction n using (well_founded_induction lt_wf).
+  intros Heven.
+  destruct n.
+  - reflexivity.
+  - destruct n.
+    + simpl in Heven. discriminate.
+    + simpl. simpl in Heven.
+      rewrite (H n). reflexivity.
+      * lia.
+      * exact Heven.
+Qed.
+
+Lemma odd_line_inverted : forall n,
+  Nat.odd n = true -> line_orientation n = Inverted.
+Proof.
+  induction n using (well_founded_induction lt_wf).
+  intros Hodd.
+  destruct n.
+  - simpl in Hodd. discriminate.
+  - destruct n.
+    + reflexivity.
+    + simpl. simpl in Hodd.
+      rewrite (H n). reflexivity.
+      * lia.
+      * exact Hodd.
+Qed.
+
+(** Reading order decidability *)
+Theorem reading_order_decidable : forall t : Tablet,
+  { well_formed_tablet t = true } + { well_formed_tablet t = false }.
+Proof.
+  intros t.
+  destruct (well_formed_tablet t) eqn:E.
+  - left. reflexivity.
+  - right. reflexivity.
+Qed.
+
+(** Parallel text alignment is decidable *)
+Theorem parallel_alignment_decidable : forall t1 t2 passage,
+  { shares_passage t1 t2 passage = true } + { shares_passage t1 t2 passage = false }.
+Proof.
+  intros t1 t2 passage.
+  destruct (shares_passage t1 t2 passage) eqn:E.
+  - left. reflexivity.
+  - right. reflexivity.
+Qed.
+
+(** * Corpus Facts *)
+
+(** Known tablets in Barthel catalog *)
+Definition tablet_A : nat := 1.   (* Tahua, ~1825 glyphs, Rome *)
+Definition tablet_B : nat := 2.   (* Aruku Kurenga, ~1290 glyphs, Rome *)
+Definition tablet_C : nat := 3.   (* Mamari, calendar sequence, Rome *)
+Definition tablet_I : nat := 9.   (* Santiago Staff, ~2320 glyphs, Chile *)
+Definition tablet_G : nat := 7.   (* Small Santiago, 31 section markers *)
+
+(** Corpus statistics *)
+Definition total_tablets : nat := 26.
+Definition core_glyph_count : nat := 120.
+Definition barthel_glyph_range : nat := 600.
+
+(** Santiago Staff has the most glyphs *)
+Definition santiago_staff_glyphs : nat := 2320.
+
+(** Small Santiago (G) has 31 occurrences of section marker 380.1.3 *)
+Definition tablet_G_section_markers : nat := 31.
+
+(** Glyph 76 appears 564 times on Santiago Staff *)
+Definition glyph_76_on_staff : nat := 564.
+
+(** Corpus fact: all orientation checking is decidable *)
+Corollary corpus_orientation_check_terminates :
+  forall t, well_formed_tablet t = true \/ well_formed_tablet t = false.
+Proof.
+  intros t.
+  destruct (well_formed_tablet t); auto.
+Qed.
